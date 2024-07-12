28c738ba441d086f5b8b763a0323ed4ef6c4f71b
leerlandais <leerlandais@gmail.com>
2024-07-12 09:56:58 +0200

    home

--- model/homeModel.php
+++ model/homeModel.php
@@ -2,6 +2,7 @@
 
 function getAllGamesForHomepage(PDO $db) : array|null {
 $sql = "SELECT * FROM jsl_games
+         WHERE jsl_game_visibility = 1
         ORDER BY jsl_game_id ASC";
 $query = $db->query($sql);
 if ($query->rowCount() === 0) return null;
