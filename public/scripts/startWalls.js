b1d9d4ca283d27402c923cb5685156f7bab0eebb
leerlandais <leerlandais@gmail.com>
2024-07-13 19:56:02 +0100

    make blocks : OK

--- public/scripts/startWalls.js
+++ public/scripts/startWalls.js
@@ -3,23 +3,24 @@ const   canvas = document.getElementById("wallbreaker"),
         ballRadius = 10,
         blockWidth = 80,
         blockHeight = 30,
+        blockSpacing = 5,
         blockRowSize = 5,
         blockColSize = 15,
+        paddleWidth = 100,
+        paddleHeight = 20,
+        totalBlockWidth = blockColSize * blockWidth + (blockColSize - 1) * blockSpacing,
         wallbreakerStartButton = document.getElementById("wallbreakerStartButton"),
         canvasWidth = (ballRadius*2) * blockWidth*0.7,
-        canvasHeight = (ballRadius*2) * blockHeight;
+        canvasHeight = (ballRadius*2) * blockHeight,
+        blockOffset = (canvasWidth - totalBlockWidth) / 2;
+
 
 canvas.setAttribute("width", canvasWidth);
 canvas.setAttribute("height", canvasHeight);
 
-
-// Pour le block
-
-
-
 // pour la raquette
 context.beginPath();
-context.rect((canvasWidth/2)-50, canvasHeight-50, 100, 20);
+context.rect((canvasWidth/2)-(paddleWidth/2), canvasHeight-(paddleHeight*3), paddleWidth, paddleHeight);
 context.strokeStyle = "black";
 context.stroke();
 context.fillStyle = "#00FF00";
@@ -28,26 +29,27 @@ context.closePath();
 
 // pour le ballon
 context.beginPath();
-context.arc(240, 160, ballRadius, 0, Math.PI * 2, false);
+context.arc((canvasWidth/2), canvasHeight-((paddleWidth)/2)-ballRadius*2, ballRadius, 0, Math.PI * 2, false);
 context.strokeStyle = "rgb(255 255 255)";
 context.stroke();
 context.fillStyle = "#8f12d7";
 context.fill();
 context.closePath();
 
+
+// pour les blocks
 const blocks = [];
-for (let c = 0; c < blockColSize; c++) {
-    blocks[c] = [];
-    for (let r = 0; r < blockRowSize; r++) {
-        blocks[c][r] = { x: 0, y: 0 };
+for (let col = 0; col < blockColSize; col++) {
+    blocks[col] = [];
+    for (let row = 0; row < blockRowSize; row++) {
+        blocks[col][row] = { x: 0, y: 0 };
     }
 }
-
 function drawBlocks() {
-    for (let col = 0; col < blockColSize; col++) {
-        for (let row = 0; row < blockRowSize; row++) {
-            const blockX = col * (blockWidth);
-            const blockY = row * (blockHeight);
+    for (let col = 1; col < blockColSize-1; col++) {
+        for (let row = 1; row < blockRowSize; row++) {
+            const blockX = blockOffset + col * (blockWidth + blockSpacing);
+            const blockY = row * (blockHeight + blockSpacing);
 
             blocks[col][row].x = blockX;
             blocks[col][row].y = blockY;
@@ -61,7 +63,6 @@ function drawBlocks() {
         }
     }
 }
+
 drawBlocks();
-
-
-
+// ADD SCREENWIDTH DETECTOR (the usual one) AND WIDTH-REACTION BASED ON IT
