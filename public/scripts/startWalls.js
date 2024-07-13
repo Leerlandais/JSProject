df7638520b5cbbd00959288f6fe30ee676b9c742
leerlandais <leerlandais@gmail.com>
2024-07-13 18:20:40 +0100

    preparation of consts for sizing everything

--- public/scripts/startWalls.js
+++ public/scripts/startWalls.js
@@ -3,6 +3,8 @@ const   canvas = document.getElementById("wallbreaker"),
         ballRadius = 10,
         blockWidth = 80,
         blockHeight = 30,
+        blockRowSize = 5,
+        blockColSize = 15,
         wallbreakerStartButton = document.getElementById("wallbreakerStartButton"),
         canvasWidth = (ballRadius*2) * blockWidth*0.7,
         canvasHeight = (ballRadius*2) * blockHeight;
@@ -14,14 +16,18 @@ canvas.setAttribute("height", canvasHeight);
 // Pour le block
 context.beginPath();
 context.rect(blockWidth, blockHeight*2, blockWidth, blockHeight);
+context.strokeStyle = "white";
+context.stroke();
 context.fillStyle = "#FF0000";
 context.fill();
 context.closePath();
 
 
-// pour le paddle
+// pour la raquette
 context.beginPath();
 context.rect((canvasWidth/2)-50, canvasHeight-50, 100, 20);
+context.strokeStyle = "black";
+context.stroke();
 context.fillStyle = "#00FF00";
 context.fill();
 context.closePath();
@@ -29,6 +35,8 @@ context.closePath();
 // pour le ballon
 context.beginPath();
 context.arc(240, 160, ballRadius, 0, Math.PI * 2, false);
-context.fillStyle = "green";
+context.strokeStyle = "rgb(255 255 255)";
+context.stroke();
+context.fillStyle = "#8f12d7";
 context.fill();
 context.closePath();
