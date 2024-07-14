9ca09321e4ed52eda7752b7cb6633ec010e04735
leerlandais <leerlandais@gmail.com>
2024-07-14 10:55:54 +0100

    bounces off paddle : OK

--- public/scripts/startWalls.js
+++ public/scripts/startWalls.js
@@ -11,26 +11,39 @@ const   canvas = document.getElementById("wallbreaker"),
         totalBlockWidth = blockColSize * blockWidth + (blockColSize - 1) * blockSpacing,
         wallbreakerStartButton = document.getElementById("wallbreakerStartButton"),
         canvasWidth = (ballRadius*2) * blockWidth*0.7,
-        canvasHeight = (ballRadius*2) * blockHeight,
+        canvasHeight = ((ballRadius*2) * blockHeight),
         blockOffset = (canvasWidth - totalBlockWidth) / 2,
         fillstyles = ["red", "blue", "green", "yellow", "black", "orange", "violet"];
 
 
 let x = canvasWidth / 2,
     y = canvasHeight-((paddleWidth)/2)-ballRadius*2,
-    dx = 3,
-    dy = -3,
-    fillstyle = fillstyles[0];
-
-
+    dx = 5,
+    dy = -5,
+    fillstyle = fillstyles[0],
+    paddlePosition = (canvas.width - paddleWidth) / 2,
+    paddleSpeed = 5,
+    gameInterval = 0;
+    rightPressed = false,
+    leftPressed = false;
 
 canvas.setAttribute("width", canvasWidth);
 canvas.setAttribute("height", canvasHeight);
 
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+
 // pour la raquette
 function drawPaddle() {
-context.beginPath();
-context.rect((canvasWidth/2)-(paddleWidth/2), canvasHeight-(paddleHeight*3), paddleWidth, paddleHeight);
+    if (rightPressed) {
+        paddlePosition = Math.min(paddlePosition + paddleSpeed, canvas.width - paddleWidth);
+    } else if (leftPressed) {
+        paddlePosition = Math.max(paddlePosition - paddleSpeed, 0);
+    }
+
+    context.beginPath();
+context.rect(paddlePosition, canvasHeight-paddleHeight, paddleWidth, paddleHeight);
 context.strokeStyle = "black";
 context.stroke();
 context.fillStyle = "#00FF00";
@@ -64,18 +77,7 @@ for (let col = 0; col < blockColSize; col++) {
         }
     }
 }
-/*
-function drawBall () {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.beginPath();
-    context.arc(x, y, 10, 0, Math.PI * 2);
-    context.fillStyle = "#0095DD";
-    context.fill();
-    context.closePath();
-    x += dx;
-    y += dy
-}
-*/
+
 function drawBall () {
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.beginPath();
@@ -92,19 +94,50 @@ function drawBall () {
         dx = -dx;
         changeColour();
     }
-    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    if (y + dy < ballRadius) {
         dy = -dy;
         changeColour();
+    } else if (y + dy > canvasHeight - ballRadius/2) {
+        if (x > paddlePosition && x < paddlePosition + paddleWidth + ballRadius/2) {
+            dy = -dy;
+            // console.log("paddleCollision");
+            changeColour();
+        } else {
+            alert("GAME OVER");
+            clearInterval(gameInterval);
+            document.location.reload();
+        }
     }
 
+
+}
 function changeColour() {
     let newColour = Math.floor(Math.random() * fillstyles.length);
     fillstyle = fillstyles[newColour];
 }
-
+function keyDownHandler(e) {
+    if (e.key === "Right" || e.key === "ArrowRight") {
+        rightPressed = true;
+        console.log("right pressed");
+    } else if (e.key === "Left" || e.key === "ArrowLeft") {
+        leftPressed = true;
+        console.log("left pressed");
+    }
 }
+
+function keyUpHandler(e) {
+    if (e.key === "Right" || e.key === "ArrowRight") {
+        rightPressed = false;
+        console.log("right depressed");
+    } else if (e.key === "Left" || e.key === "ArrowLeft") {
+        leftPressed = false;
+        console.log("left depressed");
+    }
+}
+
 function beginWallGame() {
-setInterval(() => {
+    wallbreakerStartButton.disabled = true;
+gameInterval = setInterval(() => {
     drawBall();
  //   drawBlocks();
     drawPaddle();
